0429bb78cd38bd37f5a8f1c262455447179f4649
topherson-hng <160559572+topherson-hng@users.noreply.github.com>
2024-08-26 11:16:18 -0700

    Add files via upload

--- excel_pull.py
+++ excel_pull.py
@@ -1,12 +1,17 @@
 import openpyxl
+import logging
 from openpyxl.styles import PatternFill
-from datetime import datetime, timedelta
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.utils import get_column_letter
+from openpyxl.utils.dataframe import dataframe_to_rows
+import datetime as dt
+from docx import Document
+import csv
 
 def highlight_month_dates(file_path, sheet_name, target_sheet_name="HighlightedData"):
     # Load the workbook and the specified sheet
-    wb = openpyxl.load_workbook('Path to Workbook')
+    wb = openpyxl.load_workbook(file_path)
     ws = wb['CurrentlyBidContracts']
-    # ws_source = wb[source_sheet_name]
     
     # checks if there is sheet already, if not creates a new sheet
     if target_sheet_name in wb.sheetnames:
@@ -23,12 +28,13 @@ def highlight_month_dates(file_path, sheet_name, target_sheet_name="HighlightedD
 
     # intialize target row
     target_row = 2
+    word_row = 2
 
     # Define the fill style for highlighting
     highlight_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
     # Get the current date, month, and year
-    today = datetime.today()
+    today = dt.datetime.today()
     str_today = str(today)
     date_today = str_today.replace("-","")
     new_date_today = date_today.split(' ',1)[0]
@@ -36,17 +42,21 @@ def highlight_month_dates(file_path, sheet_name, target_sheet_name="HighlightedD
     current_month = today.month
     current_year = today.year
     
+    # create word document
+    test_document = Document()
+    first_sentence = test_document.add_paragraph("Curtailment Anniversaries:")
+    first_sentence.bold = True
 
     # Iterate through the cells in column L
     for row in ws.iter_rows(min_col=12, max_col=12, min_row=1, max_row=ws.max_row):
         for cell in row:
-            if isinstance(cell.value, datetime):
+            if isinstance(cell.value, dt.datetime):
                 date_value = cell.value
                 
                 if date_value.month != current_month:
                     cell.fill = PatternFill()  # Clear the highlight
                 elif date_value.month == current_month and date_value.year == current_year:
-                    cell.fill = highlight_fill
+                    cell.fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
                     cell.value = date_value.replace(year=date_value.year + 1)
                    
             # pull highlighted cell value row "L" into separate sheet    
@@ -58,15 +68,35 @@ def highlight_month_dates(file_path, sheet_name, target_sheet_name="HighlightedD
                 ws_target.cell(row=target_row, column=4).value = ws.cell(row=cell.row, column=12).value
                 target_row += 1
 
-    for row in ws_target.iter_rows(min_col=4, max_col=4, min_row=1, max_row=ws.max_row):
+    for row in ws_target.iter_rows(min_col=4, max_col=4, min_row=1, max_row=ws_target.max_row):
         for cell in row:
             cell.number_format = 'mm/dd/yyyy;@'
 
 
-    # Save the workbook
+    max_row_for_d = max((d.row for d in ws_target['D'] if d.value is not None))
+    style = TableStyleInfo(name="TableStyleMedium9", showRowStripes=True)
+    table = Table(displayName="penguins", ref="A1:" + get_column_letter(ws_target.max_column) + str(max_row_for_d))
+    table.tableStyleInfo = style
+    ws_target.add_table(table)
+
+    # import specified cells into word file 
+    for row in ws_target.iter_rows(min_row=2, max_row=ws_target.max_row, min_col=1, max_col=ws_target.max_column):
+            log_cell = ws_target.cell(row=word_row, column=1).value
+            name_cell = ws_target.cell(row=word_row, column=2).value
+            resource_cell = ws_target.cell(row=word_row, column=3).value
+            date_cell = ws_target.cell(row=word_row, column=4).value
+            dt_cell = date_cell.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)
+            
+            if row[0].value:
+                test_document.add_paragraph(f"{name_cell} - Resource ID: {resource_cell}")
+                test_document.add_paragraph(f"The new curtailment anniversary is on {dt_cell}")
+                word_row += 1
+
+    # Save the workbook 
+    test_document.save('C:/Users/cyh3/OneDrive - PGE/Desktop/test/test.docx')
     wb.save(f'C:/Users/cyh3/OneDrive - PGE/Desktop/test/Renewable Bidding - Upcoming Contracts {yyyymmdd}.xlsx')
 
-# Usage example
-file_path = 'Path to Workbook'
+# Usage 
+file_path = r'C:/Users/cyh3/OneDrive - PGE/Desktop/test/Renewable Bidding - Upcoming Contracts 20240626.xlsm'
 sheet_name = 'CurrentlyBidContracts'
 highlight_month_dates(file_path, sheet_name)
